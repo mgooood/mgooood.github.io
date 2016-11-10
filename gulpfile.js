9880b18824eb8bd5c617eae30028cb9a6628b387
Mark Good <mark.gooood@gmail.com>
2016-11-10 18:20:10 -0500

    adding gulp workflow file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,50 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    cssnano = require('gulp-cssnano'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Other watchers
-})
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+});
+
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('.'))
+});
+
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('./images'))
+});
+
+gulp.task('clearCache', function (done) {
+  return cache.clearAll(done);
+});
