1ea1626ad198de7992bbe77c22a37ad75bdb4dfb
Mark Good <mark.gooood@gmail.com>
2016-11-11 10:30:03 -0500

    updated navigation handler and pointed work to index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-$('li', '.nav-items').on('click', function() {
-    $(this).addClass('active');
-    $(this).siblings('li').removeClass('active');
+$('a', '.nav-items').on('click', function() {
+    $(this).parent('li').addClass('active').siblings('li').removeClass('active');
 });
